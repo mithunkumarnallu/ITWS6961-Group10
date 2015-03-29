46f9d3b217c85918d713a04782023bf3db9706a7
nachi1490 <nachiket_1490@yahoo.com>
2015-03-29 17:50:50 -0400

    added stripe payments

--- instaRent/app.js
+++ instaRent/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var manageHomeRoutes = require('./routes/managehome');
+var tenantPayments = require('./routes/payments');
 
 var swig = require("swig");
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/managehome', manageHomeRoutes);
+app.use('/payments', tenantPayments);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
