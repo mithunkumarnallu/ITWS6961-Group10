0a1d4eddb5a3841e4b65bb3e2383b656f2136cf1
nachi1490 <nachiket_1490@yahoo.com>
2015-04-18 20:12:46 -0400

    some more additional functionality to payment history table

--- instaRent/public/js/paymentHistory.js
+++ instaRent/public/js/paymentHistory.js
@@ -8,6 +8,17 @@ function queryParams() {
     };
 }
 
+function rowStyle(row, index) {
+    var classes = ['active', 'success', 'info', 'warning', 'danger'];
+
+    if (index % 2 === 0 && index / 2 < classes.length) {
+        return {
+            classes: classes[index / 2]
+        };
+    }
+    return {};
+}
+
 function downloadExcel(){
     $.ajax(
         {
