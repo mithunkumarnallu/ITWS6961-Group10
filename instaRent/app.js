3fc1ee06abd97bf16f111e31e51ce6ab925783e6
mithunkumarnallu <nallum@rpi.edu>
2015-07-07 10:32:18 -0400

    Update app.js

--- instaRent/app.js
+++ instaRent/app.js
@@ -54,8 +54,8 @@ mailer.extend(app, {
     port: 465, // port for secure SMTP
     transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
     auth: {
-        user: 'noreplyinstarent@gmail.com',
-        pass: 'instarent123'
+        user: <Your email>,
+        pass: <Your password>
     }
 });
 
